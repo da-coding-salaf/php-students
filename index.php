cf6fbb633290430028653829e4602a732f6fc7c8
dacodingsalaf <wfbmail15@gmail.com>
2023-11-09 14:47:16 +0100

    9-10-23

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Form 
+	</title>
+</head>
+<body>
+<form action="backend.php" method="post">
+	<table>
+		<tr>
+			<td>
+					<input type="text" name="firstname" placeholder="firstname" required/>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<input type="text" name="username" placeholder="username" required/>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<input type="password" name="password" placeholder="password" required/>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<input type="submit" name="Login">
+			</td>
+		</tr>
+
+	</table>	
+
+</form>
+
+
+
+</body>
+</html>
