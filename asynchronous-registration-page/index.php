57da76e18842dbb23fe2b62d11544f308aef7f0d
dacodingsalaf <wfbmail15@gmail.com>
2023-11-11 14:49:26 +0100

    11-11-23

---
+++ asynchronous-registration-page/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Asynchronous Registration Page</title>
+</head>
+
+
+
+<style type="text/css">
+		body{
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			font-size: 30px;
+			height: 100vh
+		}input{
+			font-size: 30px;
+			padding: 10px
+		}
+
+
+</style>
+<body>
+
+
+
+
+<table border="1">
+	
+<form method="post" action="receiver.php">
+	
+	<tr><td style="font-weight: bold;text-align: center;text-transform: uppercase;">Registration</td></tr>
+<tr><td><input type="text"  name="first_name" placeholder="First Name"></td></tr>
+<tr><td><input type="text"  name="last_name" placeholder="Last Name"></td></tr>
+<tr><td><input type="email"  name="email" placeholder="Email"></td></tr>
+<tr><td><input type="password"  name="password" placeholder="Password"></td></tr>
+<tr><td><input type="password"  name="c_password" placeholder="Confirm Password"></td></tr>
+<tr><td><input type="submit"  step="" name="submit-btn" value ="Submit"></td></tr>
+
+
+
+</form>
+
+</table>
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ asynchronous-registration-page/receiver.php
@@ -0,0 +1,12 @@
+<?php
+$first_name=$_POST['first_name'];
+$last_name=$_POST['last_name'];
+$email=$_POST['email'];
+$password=$_POST['password'];
+$c_password=$_POST['c_password'];
+
+echo "You information has successfully been received. ".$first_name; 
+
+
+
+?>
